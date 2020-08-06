4c1850da7c7e920c1e917c1b798008bc1f873824
Heelie <Heelie.hj@gmail.com>
2020-08-06 23:14:22 +0800

    新增：RollbackCommand

--- src/Command/Migrate/RollbackCommand.php
+++ src/Command/Migrate/RollbackCommand.php
@@ -2,19 +2,95 @@
 
 namespace EasySwoole\Migrate\Command\Migrate;
 
+use EasySwoole\Command\AbstractInterface\CommandHelpInterface;
 use EasySwoole\Command\AbstractInterface\ResultInterface;
-use EasySwoole\Command\Result;
-use EasySwoole\Migrate\Command\CommandInterface;
+use EasySwoole\Command\Color;
+use EasySwoole\Migrate\Command\MigrateCommand;
+use EasySwoole\Migrate\Databases\DatabaseFacade;
+use EasySwoole\Migrate\Utility\Util;
+use Exception;
 
-class RollbackCommand extends CommandInterface
+final class RollbackCommand extends MigrateCommand
 {
-    public function exec($args = []): ResultInterface
-    {
-        if (empty($args) || is_null($args)){
+    private $dbFacade;
 
-        }
-        $result = new Result();
-        $result->setMsg('install');
-        return $result;
+    public function __construct()
+    {
+        $this->dbFacade = DatabaseFacade::getInstance();
     }
+
+    public function commandName(): string
+    {
+        return 'migrate rollback';
+    }
+
+    public function desc(): string
+    {
+        return 'database migrate rollback';
+    }
+
+    public function help(CommandHelpInterface $commandHelp): CommandHelpInterface
+    {
+        $commandHelp->addActionOpt('--batch', 'rollback migrate batch no');
+        $commandHelp->addActionOpt('--id', 'rollback migrate id');
+        return $commandHelp;
+    }
+
+    /**
+     * @return ResultInterface|string|null
+     * @throws Exception
+     */
+    public function exec(): ?string
+    {
+        $waitRollbackFiles = $this->getRollbackFiles();
+
+        $outMsg = [];
+
+        foreach ($waitRollbackFiles as $id => $file) {
+            $outMsg[] = "<brown>Migrating: </brown>{$file}";
+            $startTime = microtime(true);
+            $className = Util::migrateFileNameToClassName($file);
+            try {
+                $ref = new \ReflectionClass($className);
+                $sql = call_user_func([$ref->newInstance(), 'down']);
+                if ($this->dbFacade->query($sql)) {
+                    $deleteSql = "delete from `" . Util::DEFAULT_MIGRATE_TABLE . "` where `id`='{$id}' ";
+                    $this->dbFacade->query($deleteSql);
+                }
+            } catch (\Throwable $e) {
+                return Color::error($e->getMessage());
+            }
+            $outMsg[] = "<green>Migrated:  </green>{$file} (" . round(microtime(true) - $startTime, 2) . " seconds)";
+        }
+        $outMsg[] = "<success>Migration table rollback successfully.</success>";
+        return Color::render(implode(PHP_EOL, $outMsg));
+    }
+
+    private function getRollbackFiles()
+    {
+        $tableName = Util::DEFAULT_MIGRATE_TABLE;
+        $sql = "select `id`,`migration` from `{$tableName}` where ";
+        if (($batch = $this->getOpt('batch')) && is_numeric($batch)) {
+            $sql .= " `batch`={$batch} ";
+        } elseif (($id = $this->getOpt('id')) && is_numeric($id)) {
+            $sql .= " `id`={$id} ";
+        } else {
+            $sql .= " `batch`=(select max(batch) from `{$tableName}` )";
+        }
+        $sql .= " order by id desc";
+        $readyRollbackFiles = $this->dbFacade->query($sql);
+        if (empty($readyRollbackFiles)) {
+            return Color::success('No files to be rollback.');
+        }
+        $readyRollbackFiles = array_column($readyRollbackFiles, 'migration', 'id');
+
+        foreach ($readyRollbackFiles as $id => $file) {
+            $file = Util::MIGRATE_PATH . $file . ".php";
+            if (file_exists($file)) {
+                Util::requireOnce($file);
+            }
+        }
+        return $readyRollbackFiles;
+    }
+
 }
