6e7cae75e8d0d2e36463e7b0f085f2f57384f74b
Heelie <Heelie.hj@gmail.com>
2020-08-14 20:42:49 +0800

    修改：up GenerateCommand

--- src/Command/Migrate/GenerateCommand.php
+++ src/Command/Migrate/GenerateCommand.php
@@ -22,11 +22,26 @@ final class GenerateCommand extends MigrateCommand implements CommandInterface
     public function help(CommandHelpInterface $commandHelp): CommandHelpInterface
     {
         $commandHelp->addActionOpt('--tables', 'Generate the migration repository of the specified table, multiple tables can be separated by ","');
+        $commandHelp->addActionOpt('--ignore', 'Tables that need to be excluded when generate the migration repository, multiple tables can be separated by ","');
         return $commandHelp;
     }
 
     public function exec(): ?string
     {
         // TODO: Implement exec() method.
+    }
+
+    /**
+     * ignore tables
+     *
+     * @return array
+     */
+    protected function getIgnoreTables()
+    {
+        $ignoreTables = [Util::DEFAULT_MIGRATE_TABLE];
+        if ($ignore = $this->getOpt('ignore')) {
+            return array_merge($ignoreTables, explode(',', $ignore));
+        }
+        return $ignoreTables;
     }
 }
