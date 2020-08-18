5a1d96049f6f2631b80e950bab38a3754a782a69
Heelie <Heelie.hj@gmail.com>
2020-08-18 11:46:53 +0800

    修改：新增数据填充相关默认配置

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -8,10 +8,16 @@ class Config
     const DEFAULT_MIGRATE_TABLE = 'migrations';
 
     /** @var string migrate path */
-    const MIGRATE_PATH = EASYSWOOLE_ROOT . '/database/migrates/';
+    const MIGRATE_PATH = EASYSWOOLE_ROOT . '/Database/Migrates/';
 
     /** @var string migrate template file path */
     const MIGRATE_TEMPLATE = __DIR__ . '/../Resource/migrate._php';
+
+    /** @var string migrate template class name */
+    const MIGRATE_TEMPLATE_CLASS_NAME = 'MigratorClassName';
+
+    /** @var string migrate template table name */
+    const MIGRATE_TEMPLATE_TABLE_NAME = 'MigratorTableName';
 
     /** @var string create migrate template file path */
     const MIGRATE_CREATE_TEMPLATE = __DIR__ . '/../Resource/migrate_create._php';
@@ -22,9 +28,13 @@ class Config
     /** @var string drop migrate template file path */
     const MIGRATE_DROP_TEMPLATE = __DIR__ . '/../Resource/migrate_drop._php';
 
-    /** @var string migrate template class name */
-    const MIGRATE_TEMPLATE_CLASS_NAME = 'MigratorClassName';
 
-    /** @var string migrate template table name */
-    const MIGRATE_TEMPLATE_TABLE_NAME = 'MigratorTableName';
+    /** @var string seeder path */
+    const SEEDER_PATH = EASYSWOOLE_ROOT . '/Database/Seeds/';
+
+    /** @var string seeder template class name */
+    const SEEDER_TEMPLATE_CLASS_NAME = 'SeederClassName';
+
+    /** @var string seeder template file path */
+    const SEEDER_TEMPLATE = __DIR__ . '/../Resource/seeder._php';
 }
