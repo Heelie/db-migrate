2ae498f6f8aad2b46efd5802336d697c42b7b4a4
Heelie <Heelie.hj@gmail.com>
2020-08-22 01:03:35 +0800

    修改：完善table属性ddl生成语句

--- src/DDLSyntax/DDLTableSyntax.php
+++ src/DDLSyntax/DDLTableSyntax.php
@@ -4,6 +4,12 @@ namespace EasySwoole\Migrate\DDLSyntax;
 
 use EasySwoole\Migrate\Databases\DatabaseFacade;
 
+/**
+ * Class DDLTableSyntax
+ * @package EasySwoole\Migrate\DDLSyntax
+ * @author heelie.hj@gmail.com
+ * @date 2020/8/22 00:58:27
+ */
 class DDLTableSyntax
 {
     /**
@@ -11,8 +17,46 @@ class DDLTableSyntax
      * @param string $tableName
      * @return string
      */
-    public static function generate($tableSchema, $tableName)
+    public static function generate(string $tableSchema, string $tableName)
     {
+        $tables = self::getTableAttribute($tableSchema, $tableName);
+        return self::generateTable(current($tables));
+    }
 
+    /**
+     * @param string $tableSchema
+     * @param string $tableName
+     * @return array
+     */
+    private static function getTableAttribute(string $tableSchema, string $tableName)
+    {
+        $columns = join(',', [
+            '`TABLE_NAME`',
+            '`ENGINE`',
+            '`TABLE_COLLATION`',
+            '`AUTO_INCREMENT`',
+            '`TABLE_COMMENT`',
+        ]);
+        $sql     = "SELECT {$columns}
+                FROM `information_schema`.`TABLES` 
+                WHERE `TABLE_SCHEMA`='{$tableSchema}' 
+                AND `TABLE_NAME`='{$tableName}';";
+        return DatabaseFacade::getInstance()->query($sql);
+    }
+
+    /**
+     * @param array $table
+     * @return string
+     */
+    private static function generateTable(array $table)
+    {
+        $createTableDDl   = [];
+        $createTableDDl[] = "\$table->setIfNotExists();";
+        $createTableDDl[] = "\$table->setTableName('{$table['TABLE_NAME']}');";
+        $createTableDDl[] = "\$table->setTableEngine('{$table['ENGINE']}');";
+        $createTableDDl[] = "\$table->setTableCharset('{$table['TABLE_COLLATION']}');";
+        $createTableDDl[] = $table['AUTO_INCREMENT'] > 0 ? "\$table->setTableAutoIncrement(1);" : null;
+        $createTableDDl[] = $table['TABLE_COMMENT'] ? "\$table->setTableComment('{$table['TABLE_COMMENT']}');" : null;
+        return join(PHP_EOL, array_filter($createTableDDl));
     }
 }
