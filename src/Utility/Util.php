1d0f1a13faf3c62e44710660bc641c68aa167da5
Heelie <Heelie.hj@gmail.com>
2020-08-22 21:28:54 +0800

    修改：删除不要的配置

--- src/Utility/Util.php
+++ src/Utility/Util.php
@@ -10,33 +10,11 @@ use EasySwoole\Migrate\Validate\Validator;
 /**
  * Class Util
  * @package EasySwoole\Migrate\Utility
+ * @author heelie.hj@gmail.com
+ * @date 2020/8/22 21:28:02
  */
 class Util
 {
-    /** @var string default migrate table name */
-    const DEFAULT_MIGRATE_TABLE = 'migrations';
-
-    /** @var string migrate path */
-    const MIGRATE_PATH = EASYSWOOLE_ROOT . '/database/migrates/';
-
-    /** @var string migrate template file path */
-    const MIGRATE_TEMPLATE = __DIR__ . '/../Resource/migrate._php';
-
-    /** @var string create migrate template file path */
-    const MIGRATE_CREATE_TEMPLATE = __DIR__ . '/../Resource/migrate_create._php';
-
-    /** @var string alter migrate template file path */
-    const MIGRATE_ALTER_TEMPLATE = __DIR__ . '/../Resource/migrate_alter._php';
-
-    /** @var string drop migrate template file path */
-    const MIGRATE_DROP_TEMPLATE = __DIR__ . '/../Resource/migrate_drop._php';
-
-    /** @var string migrate template class name */
-    const MIGRATE_TEMPLATE_CLASS_NAME = 'MigratorClassName';
-
-    /** @var string migrate template table name */
-    const MIGRATE_TEMPLATE_TABLE_NAME = 'MigratorTableName';
-
     /**
      * @param string $str
      * @return string
