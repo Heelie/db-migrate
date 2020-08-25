3b42003dc15197496f1ab2103049a58ad8740df4
Heelie <Heelie.hj@gmail.com>
2020-08-25 21:23:48 +0800

    修改：DDLForeignSyntax

--- src/DDLSyntax/DDLForeignSyntax.php
+++ src/DDLSyntax/DDLForeignSyntax.php
@@ -2,6 +2,8 @@
 
 namespace EasySwoole\Migrate\DDLSyntax;
 
+use EasySwoole\Migrate\Databases\DatabaseFacade;
+
 class DDLForeignSyntax
 {
     /**
@@ -9,8 +11,27 @@ class DDLForeignSyntax
      * @param string $tableName
      * @return string
      */
-    public static function generate($tableSchema, $tableName)
+    public static function generate(string $tableSchema, string $tableName)
     {
-
+        $indAttrs = self::getForeignAttribute($tableSchema, $tableName);
+        $indAttrs = self::arrayBindKey($indAttrs, null, 'INDEX_NAME');
+        $indexDDl = array_map([__CLASS__, 'genIndexDDLSyntax'], $indAttrs);
+        return join(PHP_EOL, $indexDDl);
     }
+
+    private static function getForeignAttribute(string $tableSchema, string $tableName)
+    {
+        $columns = join(',', [
+            '`CONSTRAINT_NAME`',
+            '`COLUMN_NAME`',
+            '`REFERENCED_TABLE_NAME`',
+            '`REFERENCED_COLUMN_NAME`',
+        ]);
+        $sql     = "SELECT {$columns}
+                FROM `information_schema`.`STATISTICS` 
+                WHERE `TABLE_SCHEMA`='{$tableSchema}' 
+                AND `TABLE_NAME`='{$tableName}';";
+        return DatabaseFacade::getInstance()->query($sql);
+    }
+
 }
